382f03f68d82efa1c88b6cf8bf33566367a338c6
Boyan Yanchev <byanchev6@gmail.com>
2025-11-01 20:16:21 +0200

    Solve exercise 1

--- 02-Asynchronous-Programming/Exercises/01.Bus-Stop/app.js
+++ 02-Asynchronous-Programming/Exercises/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const inputRef = document.getElementById('stopId');
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/';
+    const busId = inputRef.value;
+
+    const stopName = document.getElementById('stopName');
+    const busUl = document.getElementById('buses');
+
+    if (!busId) {
+        return;
+    }
+
+    const response = await fetch(BASE_URL + busId);
+    const data = await response.json();
+
+    busUl.innerHTML = '';
+
+    stopName.textContent = data.name;
+    Object.entries(data.buses)
+        .forEach(([bus, time]) => {
+            const li = document.createElement('li');
+            li.textContent = `Bus ${bus} arrives in ${time} minutes`;
+            busUl.appendChild(li);
+        })
 }
