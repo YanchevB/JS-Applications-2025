52a008dbe5fa5cbc16604ebe0282484d664723a5
Boyan Yanchev <byanchev6@gmail.com>
2025-11-04 19:51:12 +0200

    Solve exercise 2

--- 02-Asynchronous-Programming/Exercises/02.Bus-Schedule/app.js
+++ 02-Asynchronous-Programming/Exercises/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    const info = document.getElementById('info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    const stopInfo = {
+        name: '',
+        nextStop: 'depot'
+    }
+
+    async function depart() {
+        try {
+            const response = await fetch(BASE_URL + stopInfo.nextStop);
+            const data = await response.json();
+            stopInfo.name = data.name;
+            stopInfo.nextStop = data.next;
+            info.textContent = `Next stop: ${stopInfo.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            info.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${stopInfo.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
