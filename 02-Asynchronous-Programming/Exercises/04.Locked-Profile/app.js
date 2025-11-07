1241563bb8a538c16c632fd90ef64ff20ec534c7
Boyan Yanchev <byanchev6@gmail.com>
2025-11-07 13:06:05 +0200

    Solve exercise 4 and create a zip file for submission

--- 02-Asynchronous-Programming/Exercises/04.Locked-Profile/app.js
+++ 02-Asynchronous-Programming/Exercises/04.Locked-Profile/app.js
@@ -1,3 +1,64 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const main = document.getElementById('main');
+
+    const BASE_URL = 'http://localhost:3030/jsonstore/advanced/profiles';
+    const options = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const response = await fetch(BASE_URL, options);
+    const data = await response.json();
+
+    main.innerHTML = '';
+
+    Object.entries(data).forEach(([id, body]) => {
+        main.innerHTML += `
+            <div class="profile">
+                <img src="./iconProfile2.png" class="userIcon" />
+                <label>Lock</label>
+                <input type="radio" name="user${id}Locked" value="lock" checked>
+                <label>Unlock</label>
+                <input type="radio" name="user${id}Locked" value="unlock"><br>
+                <hr>
+                <label>Username</label>
+                <input type="text" name="user${id}Username" value="${body.username}" disabled readonly />
+                <div class="user1Username">
+                    <hr>
+                    <label>Email:</label>
+                    <input type="email" name="user${id}Email" value="${body.email}" disabled readonly />
+                    <label>Age:</label>
+                    <input type="number" name="user${id}Age" value="${body.age}" disabled readonly />
+                </div>
+
+                <button>Show more</button>
+            </div>`
+    })
+
+    const userInfo = document.querySelectorAll('div .user1Username');
+    Array.from(userInfo).forEach(user => user.style.display = 'none');
+
+    const btns = document.querySelectorAll('button');
+    Array.from(btns).forEach(button => button.addEventListener('click', onClick));
+
+    function onClick(e) {
+        const button = e.target;
+        const profile = e.target.parentElement;
+        const userInfo = profile.querySelector('.user1Username');
+        const selectedValue = profile.querySelector('input[type="radio"]:checked')?.value;
+
+        if (selectedValue === 'lock') {
+            return;
+        }
+
+        if (button.textContent === 'Show more') {
+            userInfo.style.display = 'block';
+            button.textContent = 'Hide it';
+        } else if (button.textContent === 'Hide it'){
+            userInfo.style.display = 'none';
+            button.textContent = 'Show more';
+        }
+    }
 }
