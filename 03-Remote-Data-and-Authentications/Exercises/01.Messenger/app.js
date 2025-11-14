5ee4408212924abf56270a0fb6b8b6a988020aa2
Boyan Yanchev <byanchev6@gmail.com>
2025-11-14 20:23:16 +0200

    Solve exercise 1

--- 03-Remote-Data-and-Authentications/Exercises/01.Messenger/app.js
+++ 03-Remote-Data-and-Authentications/Exercises/01.Messenger/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
+    const textarea = document.getElementById('messages');
+    const authorRef = document.querySelector('input[name="author"]');
+    const messageRef = document.querySelector('input[name="content"]');
+
+    document.getElementById('submit').addEventListener('click', onSubmit);
+    document.getElementById('refresh').addEventListener('click', onRefresh);
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        const author = authorRef.value;
+        const content = messageRef.value;
+
+        if (!author || !content) {
+            return
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({author, content})
+        }
+
+        const response = await fetch(BASE_URL, options);
+
+        authorRef.value = '';
+        messageRef.value = '';
+    }
+
+    async function onRefresh(e) {
+        e.preventDefault();
+        const response = await fetch(BASE_URL);
+
+        if (response.status !== 200) {
+            return;
+        }
+
+        const data = await response.json();
+
+        let buff = ''
+        Object.values(data).forEach(({author, content}) => buff += `${author}: ${content}\n`)
+        
+        textarea.value = buff.trim();
+    }
+
 }
 
 attachEvents();
