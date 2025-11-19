7fd3e0869e9ddcc7124783d985f25832e25cc681
Boyan Yanchev <byanchev6@gmail.com>
2025-11-19 20:23:52 +0200

    Solve exercise 3

--- 03-Remote-Data-and-Authentications/Exercises/03.Students/app.js
+++ 03-Remote-Data-and-Authentications/Exercises/03.Students/app.js
@@ -1,1 +1,65 @@
-console.log('TODO...');
+const tbody = document.querySelector('tbody');
+const studentsUrl = 'http://localhost:3030/jsonstore/collections/students';
+
+window.addEventListener('load', onLoad);
+document.getElementById('form').addEventListener('submit', onSubmit);
+
+async function onLoad(e) {
+  const response = await fetch(studentsUrl);
+  const data = await response.json();
+
+  tbody.innerHTML = '';
+  Object.values(data).forEach(student => {
+    const tr = document.createElement('tr');
+    
+    tr.innerHTML = `
+      <td>${student.firstName}</td>
+      <td>${student.lastName}</td>
+      <td>${student.facultyNumber}</td>
+      <td>${student.grade}</td>
+    `;
+
+    tbody.appendChild(tr);
+  })
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+  const notification = document.querySelector('.notification')
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+  
+  const {firstName, lastName, facultyNumber, grade} = data;
+
+  if (!firstName || !lastName || !facultyNumber || !grade) {
+    notification.textContent = 'All fields are required!';
+    return;
+  }
+
+  if (isNaN(facultyNumber) || isNaN(grade)) {
+    notification.textContent = 'Faculty Number and Grade must be numbers!';
+    return;
+  }
+
+  const newStudent = {
+    firstName,
+    lastName,
+    facultyNumber,
+    grade
+  }
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newStudent)
+  }
+
+  await fetch(studentsUrl, options);
+  
+  onLoad();
+
+  notification.textContent = '';
+  e.target.reset();
+}
