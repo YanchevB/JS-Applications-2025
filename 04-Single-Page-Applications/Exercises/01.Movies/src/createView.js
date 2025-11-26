2c44cc97ec4a9db5be6973fba8c5074b65104bb8
Boyan Yanchev <byanchev6@gmail.com>
2025-11-27 00:44:17 +0200

    Add submit functionality

--- 04-Single-Page-Applications/Exercises/01.Movies/src/createView.js
+++ 04-Single-Page-Applications/Exercises/01.Movies/src/createView.js
@@ -1,8 +1,26 @@
+import { dataService } from "./dataService.js";
+import { showHome } from "./homeView.js";
 
 const sections = document.querySelectorAll('section');
 const createSection = document.getElementById('add-movie');
+document.getElementById('add-movie-form').addEventListener('submit', onSubmit);
 
-export function showCreateView() {
+export function showCreateView(e) {
+  e.preventDefault();
   sections.forEach(section => section.style.display = 'none');
   createSection.style.display = 'block';
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  const {title, description, img} = Object.fromEntries(formData);
+
+  if (!title, !description, !img) {
+    return;
+  }
+
+  await dataService.addMovie({title, description, img});
+  showHome();
 }
