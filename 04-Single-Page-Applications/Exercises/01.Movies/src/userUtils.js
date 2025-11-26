9d5bbc92922407103779c066d39b779f4636bf95
Boyan Yanchev <byanchev6@gmail.com>
2025-11-27 00:47:39 +0200

    Create user utils handlers

---
+++ 04-Single-Page-Applications/Exercises/01.Movies/src/userUtils.js
@@ -0,0 +1,33 @@
+
+
+function saveUserData(userData) {
+  const accessToken = userData.accessToken;
+  const email = userData.email;
+  const id = userData._id
+  sessionStorage.setItem('userData', JSON.stringify({accessToken, email, id}));
+}
+
+function getUserData() {
+  return JSON.parse(sessionStorage.getItem('userData'));
+}
+
+function clearUserData() {
+  sessionStorage.clear('userData');
+}
+
+function getId() {
+  const userData = getUserData();
+  return userData.id;
+}
+
+function isOwner(itemId) {
+  return itemId === getId();
+}
+
+export const userUtils = {
+  saveUserData,
+  getUserData,
+  getId,
+  clearUserData,
+  isOwner
+}
