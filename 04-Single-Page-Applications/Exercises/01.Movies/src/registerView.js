34a050ba091c19e77889f4fc64003aff68d4ae4a
Boyan Yanchev <byanchev6@gmail.com>
2025-11-26 11:27:32 +0200

    Start working on user register functionality

---
+++ 04-Single-Page-Applications/Exercises/01.Movies/src/registerView.js
@@ -0,0 +1,24 @@
+import { userService } from "./userService.js";
+
+const sections = document.querySelectorAll('section');
+const registerSection = document.getElementById('form-sign-up');
+
+document.getElementById('register-form').addEventListener('submit', onSubmit);
+
+export function showRegisterView() {
+  sections.forEach(section => section.style.display = 'none');
+  registerSection.style.display = 'block';
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const {email, password, repeatPassword} = Object.fromEntries(formData);
+  
+  if (!email || !password || password.length < 6 || password !== repeatPassword) {
+    return alert('Error');
+  }
+
+  const userData = await userService.register({email, password});
+  debugger;
+}
