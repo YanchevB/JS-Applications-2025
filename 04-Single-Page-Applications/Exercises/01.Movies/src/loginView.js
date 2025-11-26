c5599d33727dfa14bdbc982f2ce33b251c22b1ed
Boyan Yanchev <byanchev6@gmail.com>
2025-11-27 00:45:54 +0200

    Add data submission during login functionality

--- 04-Single-Page-Applications/Exercises/01.Movies/src/loginView.js
+++ 04-Single-Page-Applications/Exercises/01.Movies/src/loginView.js
@@ -1,8 +1,27 @@
+import { showHome } from "./homeView.js";
+import { userService } from "./userService.js";
+import { userUtils } from "./userUtils.js";
 
 const sections = document.querySelectorAll('section');
 const loginSection = document.getElementById('form-login');
+document.getElementById('form-login').addEventListener('submit', onSubmit);
 
 export function showLoginView() {
   sections.forEach(section => section.style.display = 'none');
   loginSection.style.display = 'block';
+}
+
+async function onSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+
+  const {email, password} = Object.fromEntries(formData);
+
+  if (!email || !password) {
+    return alert('Error');
+  }
+
+  const userData = await userService.login({email, password});
+  userUtils.saveUserData(userData);
+  showHome()
 }
